2945911e76c72302646e823cbf285dc8c9fc01ea
mason.hows11 <mason.hows11@gmail.com>
2025-01-18 21:33:23 +0330

    remove old codes

--- app/Livewire/DieWithMePage.php
+++ app/Livewire/DieWithMePage.php
@@ -22,24 +22,22 @@ class DieWithMePage extends Component
 
     public function mount()
     {
-        $this->unique_identifier = v6();
+
     }
 
     public function send(string $message)
     {
-        broadcast(new MessageSent($this->unique_identifier, $this->display_name, $message))->toOthers();
+
     }
 
     public function typing()
     {
-        $this->isTyping = true;
-        broadcast(new IsTyping($this->unique_identifier, $this->display_name))->toOthers();
+
     }
 
     public function idle()
     {
-        $this->isTyping = false;
-        broadcast(new TypingStopped($this->unique_identifier, $this->display_name))->toOthers();
+
     }
 
     public function render()
