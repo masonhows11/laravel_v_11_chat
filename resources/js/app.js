0145ab69c4901023a87f712c847c80ab4b2f43c0
mason.hows11 <mason.hows11@gmail.com>
2025-01-19 19:00:52 +0330

    websocket worked fined but..

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,20 @@ console.log('app started')
 console.log(Echo)
 // for test reverb channel in client side
 // in listen() arg is event className
-// ini this test is ClickEvent event class
+// in this test is ClickEvent event class name
+// that defined in laravel project
 Echo.channel('test-channel').listen('ClickEvent', testClickEvent);
 
-function testClickEvent(event)
-{
-    console.log(event);
+function testClickEvent(event) {
+    // console.log(event);
+    // console.log(event.text);
+    // console.log(
+    //     document.getElementById(`counter-${event.text}`)
+    // )
+
+
+    const counterEl = document.getElementById(`counter-${event.text}`);
+    let counter = parseInt(counterEl.innerText);
+    counter++;
+    counterEl.innerText = `${counter}`;
 }
